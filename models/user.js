806b5e7e8ef02bb8d56e3982fdf07ad26d0a3ed0
Jorge L <jorge.alfmur@gmail.com>
2018-11-21 02:57:52 -0500

    modelo user

---
+++ models/user.js
@@ -0,0 +1,41 @@
+const Sequelize = require('sequelize');
+const db = require('../db/db');
+
+const User = db.define('user', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  lastname: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  nickname: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  birthday: {
+    type: Sequelize.DATE,
+    allowNull: false,
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      isEmail: true,
+    },
+  },
+  password: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      min: 6,
+    },
+  },
+  avatar: {
+    type: Sequelize.STRING,
+    allowNull: true,
+  },
+});
+
+module.exports = User;
