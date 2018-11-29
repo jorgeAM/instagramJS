3f1fe84d40d8b67b35b4e39a11d3fccb41a9ede2
Jorge L <jorge.alfmur@gmail.com>
2018-11-28 23:26:58 -0500

    subir imagen a publicación

--- routes/publication.js
+++ routes/publication.js
@@ -94,4 +94,44 @@ app.delete('/publications/:id', jwtMiddleware, async (req, res) => {
   }
 });
 
+app.post('/publications/:id/upload', [jwtMiddleware, multipartMiddleware], async (req, res) => {
+  const { id } = req.params;
+  if (req.files) {
+    const type = req.files.image.type;
+    const file = req.files.image.path.split('/')[2];
+    if (type == 'image/jpeg' || type == 'image/png') {
+      try {
+        const publication = await Publication.findOne({
+          where: { id },
+        });
+        if (!publication) {
+          deleteImage('publications', file);
+          return res.status(400).json({ message: 'Publicación no encontrada' });
+        }
+
+        if (publication.userId !== req.auth.id) {
+          deleteImage('publications', file);
+          return res.status(401).json({ message: 'Esta publicación no es tuya' });
+        }
+
+        if (publication.image) {
+          deleteImage('publications', publication.image);
+        }
+
+        publication.image = file;
+        const updatedPublication = await publication.save();
+        res.status(200).json({ publication: updatedPublication });
+      } catch (err) {
+        deleteImage('publications', file);
+        res.status(500).json({ err });
+      }
+    } else {
+      deleteImage('publications', file);
+      res.status(400).json({ message: 'Solo puede subir archivos JPG o PNG' });
+    }
+  } else {
+    res.status(400).json({ message: 'Sube una imagen' });
+  }
+});
+
 module.exports = app;
